6fe63bb16f9831d8048fa3df04ab886cb4e9a8b6
Gar <gar@lonelydatu.com>
2019-06-21 11:14:13 -0400

    bb

--- dev/std_300x250/_js/main.js
+++ dev/std_300x250/_js/main.js
@@ -4,10 +4,10 @@ import {flicker, range} from '../../_common/js/common.js'
 TweenLite.defaultEase = Power3.easeInOut
 const tl = new TimelineMax()
 tl.to('.frame1', .5, {x:-300}, "+=2")
-tl.to('.frame1', .5, {x:-600}, "+=4")
-tl.from('.bar_cta', .7, {y:"+=80"}, "+=.3")
+tl.to('.frame1', .5, {x:-600}, "+=1")
+// tl.from('.bar_cta', .7, {y:"+=80"}, "+=.3")
 
-// tl.to('.frame1', .5, {x:-900}, "+=2")
+tl.to('.frame1', .5, {x:-900}, "+=2")
 
 flicker({
 	time: .2,
